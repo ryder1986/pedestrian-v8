f3cf5df71d7c8fedd825bb43b09b2a0b997af073
root <you@example.com>
2017-10-30 10:24:53 +0800

    1

--- client/yuvrender.h
+++ client/yuvrender.h
@@ -32,27 +32,6 @@ public:
 
         memset(frame.data,100,VIDEO_WIDTH*VIDEO_HEIGHT*3);
     }
-
-
-    //    void  set_buf(char *buffer)
-    //    {
-    //        memcpy(yuv_buf,buffer,VIDEO_WIDTH*VIDEO_HEIGHT*3/2);
-    //    }
-
-    //    void  initializeGL()
-    //    {
-
-    //    }
-
-    //    void  resizeGL(int w, int h)
-    //    {
-
-    //    }
-
-
-
-
-
     static unsigned char  CONVERT_ADJUST(double tmp)
     {
         return (unsigned char)((tmp >= 0 && tmp <= 255)?tmp:(tmp < 0 ? 0 : 255));
@@ -151,8 +130,8 @@ public:
         imwrite("test.jpg",rgb_frame);
         int test=*rgb_frame.data;
 
-       // if(20<test<128){
-         //   if(1){
+        // if(20<test<128){
+        //   if(1){
         ////     cvtColor(rgb_frame,yuv_frame,CV_RGB2GRAY);
         cvtColor(rgb_frame,yuv_frame,CV_BGR2RGB);
         QImage  img = QImage((const uchar*)(yuv_frame.data),
@@ -160,7 +139,7 @@ public:
                              yuv_frame.cols*yuv_frame.channels(),
                              QImage::Format_RGB888);
         painter.drawImage(QRect(0,0,this->width(),this->height()),img);
-     //   }
+        //   }
         painter.endNativePainting();
 
 #endif
@@ -223,60 +202,6 @@ public:
         }
         //  paint_layout2(painter);
     }
-    //protected:
-    //    void paintEvent(QPaintEvent *)
-    //    {
-    //        qDebug()<<"paint "<<tick++;
-    //        tick+=10;
-    //        QPainter painter(this);
-
-
-
-    //        painter.beginNativePainting();
-    //        makeCurrent();
-    //        CONVERT_YUV420PtoRGB24((unsigned char *)yuv_buf,(unsigned char *)rgb_buf,video_width,video_height);
-    //        glPixelZoom(1.0f*this->width()/video_width, 1.0f*this->height()/video_height);
-    //#if 0
-    //        glDrawPixels(video_width, video_height,GL_RGB, GL_UNSIGNED_BYTE, rgb_buf);
-    //#else
-    //        QImage img((uchar *)rgb_buf,video_width,video_height,QImage::Format_RGB888);
-    //        //  QImage img((uchar *)rgb_buf,video_width,video_height,QImage::Format_RGB888);
-    //        //QImage  img = QImage((const uchar*)(frame.data),frame.cols,frame.rows,frame.cols*frame.channels(),QImage::Format_Indexed8);
-    //        //   QImage  img = QImage((const uchar*)(frame.data),frame.cols,frame.rows,frame.cols*frame.channels(),QImage::Format_Grayscale8);
-
-    //        painter.drawImage(QRect(0,0,this->width(),this->height()),img);
-    //#endif
-    //        painter.endNativePainting();
-
-
-
-    //        QBrush red_brush_trans(QColor(0,0,200,100));
-    //        painter.setBrush(red_brush_trans);
-    //        // pressed_x=0;
-    //        // pressed_y=0;
-    //        //  if(flg++>10)
-
-
-    //        painter.drawEllipse(pressed_x+tick%300,pressed_y,100,100);
-    //        QPen testpen(red_brush_trans,10);
-    //        painter.setPen(testpen);
-
-    //        for(int i=0;i<8;i++){
-    //            painter.drawLine(pt[i*2],pt[i*2+1]);
-    //        }
-
-
-    //        for(int i=0;i<8;i++){
-    //            painter.drawLine(pt[i*2],pt[i*2+1]);
-    //        }
-
-    //        for(int i=0;i<8;i++){
-    //            painter.drawLine(pt_rear[i*2],pt_rear[i*2+1]);
-    //        }
-    //        painter.drawEllipse(p1,10,10);
-    //        painter.drawEllipse(p2,10,10);
-    //        painter.drawEllipse(p3,10,10);
-    //    }
     void  initializeGL()
     {
 
@@ -288,30 +213,12 @@ public slots:
     int render_set_mat(Mat frame_mat)
     {
         int size=frame_mat.total();
-        //  prt(info,"%d",size);
-        // char tmp2=*f.data;
-        // prt(info,"tick %d",tickmat++);
-        //        if(tickmat >590&&tickmat<610)
-        //            return 1;
-        //   prt(info,"render set frame  ");
         if(size>0)
         {
-            //     prt(info,"render set frame ok");
             frame=frame_mat;
-            //       prt(info,"%d",sizeof(f.data));
-            //char tmp1=*f.data;
-            //char tmp=*frame.data;
         }else
         {
-
             prt(info,"render set frame fail");
-//            frame.resize(640*480*3);
-//            frame=Mat(640,480,CV_8UC3);
-
-//            memset(frame.data,100,640*480*3);
-            //    char tmp1=*f.data;
-            //   char tmp=*frame.data;
-
         }
         this->update();
         //    prt(info,"update pic menually");
@@ -320,16 +227,16 @@ public slots:
 
 private:
     int tmp_tick;
-//    char yuv_buf[640*480*3/2];
-//    char rgb_buf[640*480*3];
-//    char rgb_buf_tmp[640*480*3];
+    //    char yuv_buf[640*480*3/2];
+    //    char rgb_buf[640*480*3];
+    //    char rgb_buf_tmp[640*480*3];
     int video_width;
     int video_height;
     int pressed_x,pressed_y,pressed;
     int tick;
     int tickmat;
     Mat frame;
- //   Mat empty_frame;
+    //   Mat empty_frame;
     int pos_x;
     int pos_y;
 
