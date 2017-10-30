4b3443f9106834a4676c769e79b2eb426fc0323c
root <you@example.com>
2017-10-30 15:14:18 +0800

    1

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,42 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-//#include "client.h"
-//#include "camera.h"
-//MainWindow::MainWindow(QWidget *parent) :
-//    QMainWindow(parent),
-//    ui(new Ui::MainWindow)
-//{
-//    ui->setupUi(this);
-//    ui->groupBox->setFixedWidth(100);
-//    ui->centralWidget->setLayout(ui->horizontalLayout);
-//    ui->groupBox->setLayout(ui->gridLayout);
-//    ui->widget->setLayout(ui->gridLayout_2);
-
-//    cam_manager=new CameraManager();
-
-////    clt=new client();
-////    //CameraManager *cam=new CameraManager();
-////    render=new YuvRender();
-////    clt->get_client_setting();
-////    Config *cfg=new Config;
-////    Camera *cam=new Camera(cfg->data.camera[0]);
-////    connect(&cam->src,SIGNAL(frame_update(Mat)),render,SLOT(set_mat(Mat) ));
-//#if 0
-//    QPushButton *b1=new QPushButton("1");
-//    QPushButton *b2=new QPushButton("2");
-//    QPushButton *b3=new QPushButton("3");
-//    ui->gridLayout_2->addWidget(b1,1,1);
-//    ui->gridLayout_2->addWidget(b2,2,1);
-//    ui->gridLayout_2->addWidget(b3,2,2);
-//#endif
-// }
-
-//MainWindow::~MainWindow()
-//{
-//          delete cam_manager;
-//    delete ui;
-//}
-
 void MainWindow::on_pushButton_search_device_clicked()
 {
     client->search_device();
@@ -45,7 +8,6 @@ void MainWindow::on_pushButton_search_device_clicked()
 
 void MainWindow::on_pushButton_connect_server_clicked()
 {
-  //  client->server_ip=ui->lineEdit_connect->text();
     client->server_ip=ui->lineEdit_search->text();
     ui->lineEdit_connect->setText( client->server_ip);
     client->connect_to_server();
@@ -59,11 +21,7 @@ void MainWindow::on_pushButton_get_config_clicked()
     rst=rst.remove(0,Protocol::HEAD_LENGTH);//TODO:get the ret value
 
     cam_manager->save_config(rst);
-    // cam_manager->cfg->set_ba(rst);
     cam_manager->reconfig_camera(ui->gridLayout_2);
-    //  cam_manager->cfg->save();
-    // YuvRender *r=new YuvRender();
-    //  connect(cam_manager->cams,SIGNAL())
 
     ui->lineEdit_get->setText( cam_manager->get_config());
 }
