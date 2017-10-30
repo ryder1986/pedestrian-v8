cacde174eca178b5d3f1247ba835e2ae571a87bd
root <you@example.com>
2017-10-30 15:30:07 +0800

    1

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -16,8 +16,8 @@ void MainWindow::on_pushButton_connect_server_clicked()
 
 void MainWindow::on_pushButton_get_config_clicked()
 {
-    int request_length=Protocol::encode_configuration_request(buf);
-    QByteArray rst=client->call_server(buf,request_length);
+    int request_length=Protocol::encode_configuration_request(buf);//encoder buffer
+    QByteArray rst=client->call_server(buf,request_length);//talk to server
     rst=rst.remove(0,Protocol::HEAD_LENGTH);//TODO:get the ret value
 
     cam_manager->save_config(rst);
@@ -32,11 +32,11 @@ void MainWindow::on_pushButton_get_config_clicked()
 void MainWindow::on_pushButton_add_clicked()
 {
     QString ip=ui->lineEdit_add->text();
-    cam_manager->add_camera(ip);
-    QByteArray setting=cam_manager->get_config(1);
-    int len=Protocol::encode_addcam_request(buf,setting.length());
+    cam_manager->add_camera(ip);//add camera on client
+    QByteArray setting=cam_manager->get_config(1);//get new config from local database
+    int len=Protocol::encode_addcam_request(buf,setting.length());//encode buffer
     memcpy(buf+Protocol::HEAD_LENGTH,setting.data(),setting.length());
-    QByteArray rst=client->call_server(buf,len);
+    QByteArray rst=client->call_server(buf,len);//talk to server
     cam_manager->reconfig_camera(ui->gridLayout_2);
 }
 
@@ -47,9 +47,9 @@ void MainWindow::on_pushButton_del_clicked()
     QString index=ui->lineEdit_del->text();
     if(index.toInt()<=cam_manager->get_size()&&index.toInt()>0)
     {
-        cam_manager->del_camera(index.toInt());
-        Protocol::encode_delcam_request(buf,index.toInt());
-        client->call_server(buf,Protocol::HEAD_LENGTH);
+        cam_manager->del_camera(index.toInt());// delete camera local
+        Protocol::encode_delcam_request(buf,index.toInt());//encode buffer
+        client->call_server(buf,Protocol::HEAD_LENGTH);//talk to server
     }
      cam_manager->reconfig_camera(ui->gridLayout_2);
 }
